3a243a58def19e64f4cd0ca55d659c2cd72f21ab
ar-subham.routray <ar-subham.routray@rakuten.com>
2020-08-12 03:14:12 +0530

    Updates

---
+++ precache-manifest.078837d8616aa28154d696ed2dab591e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2b57a0e7b37e8719f38aa6304ef427",
+    "url": "/Navigus-presence-service/index.html"
+  },
+  {
+    "revision": "d6d68c0a46a625357794",
+    "url": "/Navigus-presence-service/static/js/2.7cd66521.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Navigus-presence-service/static/js/2.7cd66521.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "308b9cc196da8b9fe0ae",
+    "url": "/Navigus-presence-service/static/js/main.d250b904.chunk.js"
+  },
+  {
+    "revision": "a005ed26c1a2ba7de3a1",
+    "url": "/Navigus-presence-service/static/js/runtime-main.42442270.js"
+  },
+  {
+    "revision": "b38f3d96d0cd8158707985666697ebd0",
+    "url": "/Navigus-presence-service/static/media/rightnavigus.b38f3d96.jpg"
+  }
+]);
