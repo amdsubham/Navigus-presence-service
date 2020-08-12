704508025cdc37a4c0a919906a4414470c988f14
Subham Routray <sub.subham9574@gmail.com>
2020-08-12 09:52:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import React from "react";
 import CreateAccount from "./account/CreateAccount";
 import LoginPage from "./account/LoginPage";
 import HomePage from "./HomePage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomeLogin from "./account/HomeLogin";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <BrowserRouter history={history} basename={process.env.PUBLIC_URL}>
-
-        <Switch>
-          <Route path="/create-account" component={CreateAccount} />
-          <Route path="/login" component={LoginPage} />
-          <Route path="/" component={HomeLogin} />
-          <Route path="/home" component={HomePage} />
-        </Switch>
-        </BrowserRouter>
-    </Router>
+      <Switch>
+        <Route path="/create-account" component={CreateAccount} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/" component={HomeLogin} />
+        <Route path="/home" component={HomePage} />
+      </Switch>
+</Router>
   );
 }
 
